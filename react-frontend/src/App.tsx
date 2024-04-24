21b1142fac577d7a038742ac45bd953e6eaa06b3
Ranajit Roy <ranajit.roy@colorado.edu>
2024-04-23 23:59:08 -0600

    crime List updated

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -2,19 +2,10 @@ import React, { useState } from 'react';
 import './App.css';
 import Search from './search/Search';
 import { GlobalState } from './types';
+import { getAllData } from './api-interface';
 
-// type TGlobalState = {
-//   num1: number | null,
-//   num2: number | null,
-//   product: number | null
-// }
 
 function App() {
-  // const [state, setState] = useState<TGlobalState>({
-  //   num1: null,
-  //   num2: null,
-  //   product: null
-  // })
 
   const [globalState, setGlobalState] = useState<GlobalState>({
     filters: {
@@ -26,54 +17,19 @@ function App() {
     crimeList: []
   });
 
-  const [dataDump, setData] = useState<string>("")
-
-
-  // const handleClick = () => {
-  //   fetch(`/api/multiply/${state.num1}/${state.num2}`)
-  //     .then(response => response.json())
-  //     .then(json => setState(prevState => {
-  //       return {
-  //         ...prevState,
-  //         product: json["data"]
-  //       }
-  //     }))
-  //     .catch(error => console.log(error));
-  // }
-
   const handleDump = () => {
-    fetch(`/api/alldata`)
-      .then(response => response.json())
-      .then(json => {
-        console.log(json["data"])
-        setData(JSON.stringify(json["data"], null, 2))
-      })
-      .catch(error => console.log(error));
+    getAllData({
+      globalState: globalState,
+      setGlobalState: setGlobalState
+    })
   }
-
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setState(prevState => {
-  //     return {
-  //       ...prevState,
-  //       [event.target.name]: event.target.value
-  //     }
-  //   })
-  // }
 
   return (
     <div className="App">
       <Search globalState={globalState} setGlobalState={setGlobalState}/>
-
-      {/* <div className="MultiplySection">
-        <p>Multiply:</p>
-        <input onChange={handleChange} type='number' id="number1-inp" name="num1" value={state.num1 === null? "":state.num1 as number}/>
-        <input onChange={handleChange} type='number' id="number2-inp" name="num2" value={state.num2 === null? "":state.num2 as number}/>
-        <button onClick={handleClick}>Check</button>
-        <p id="multiply-results">{state.product}</p>
-      </div> */}
       <div className="DataDump">
         <button onClick={handleDump}>Database Dump</button>
-        <pre id="dump">{dataDump}</pre>
+        <pre id="dump">{JSON.stringify(globalState.crimeList, null, 2)}</pre>
       </div>
     </div>
   );
