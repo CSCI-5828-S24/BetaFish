9d6b7da2b9ef0cc8eea23385c26df55a36146f4f
Ranajit Roy <ranajit.roy@colorado.edu>
2024-04-24 04:12:24 -0600

    added params for API call

--- react-frontend/src/api-interface.ts
+++ react-frontend/src/api-interface.ts
@@ -2,8 +2,12 @@ import { GlobalStateProps } from "./types";
 
 const getAllData = (props: GlobalStateProps) => {
     fetch(`/api/alldata?` + new URLSearchParams({
+        startTime: new Date(props.globalState.filters.startDate).getTime().toString(),
+        endTime: new Date(props.globalState.filters.endDate).getTime().toString(),
+        lat: props.globalState.filters.lat.toString(),
+        long: props.globalState.filters.long.toString(),
         pageno: props.globalState.crimeList.page_no.toString(),
-        pagesize: props.globalState.crimeList.page_size.toString()
+        pagesize: props.globalState.crimeList.page_size.toString(),
     }))
       .then(response => response.json())
       .then(json => {
