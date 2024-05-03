4bb8b0fa3f8ea5a316fc64c964cf1cdd683dfc76
Ranajit Roy <ranajit.roy@colorado.edu>
2024-05-03 14:23:08 -0600

    monitoring added

--- flask-backend/src/flask_server.py
+++ flask-backend/src/flask_server.py
@@ -2,7 +2,9 @@ from functools import cmp_to_key
 from flask import Flask, request, Response, g
 from flask_cors import CORS
 import jsonpickle
-import os
+import os, time
+import prometheus_client
+from prometheus_client.metrics import Histogram, Counter
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -15,14 +17,19 @@ app = Flask(__name__, static_folder="../../react-frontend/build", static_url_pat
 CORS(app, origins=["*"])
 
 def create_app():
+    _INF = float("inf")
     app = Flask(__name__, static_folder="../../react-frontend/build", static_url_path='/')
-
 
     app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
     app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
     app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
     app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
- 
+
+    graph = {}
+    graph['count'] = Counter('total_req', 'total requests on all paths')
+    graph['analytics_latency'] = Histogram('analytics_latency', 'Latency for anaytics API', buckets=(100, 200, 300, _INF))
+    graph['data_latency'] = Histogram('data_latency', 'Latency for data API', buckets=(100, 200, 300, _INF))
+    
     def getDB():
         if 'db' not in g or not g.db.is_connected():
             g.db = mysql.connector.connect(
@@ -41,6 +48,7 @@ def create_app():
     """
     @app.route('/')
     def index():
+        graph['count'].inc()
         return app.send_static_file('index.html')
 
     """
@@ -55,10 +63,12 @@ def create_app():
             }
         except Exception as error:
             response = {
-                'error' : error 
+                'error' : error,
+                'data': False
             }
             status = 500
         response_pickled = jsonpickle.encode(response)
+        graph['count'].inc()
         return Response(response=response_pickled, status=status, mimetype='application/json')
 
     # @app.route('/api/multiply/<int:x>/<int:y>', methods=["GET"])
@@ -78,16 +88,17 @@ def create_app():
     
     @app.route('/api/crime_freq', methods=["GET"])
     def get_crime_freq():
+        start = time.time_ns()
+        graph['count'].inc()
         status = 200
         try:
             mydb = getDB()
             cursor = mydb.cursor()
             queryToExecute = f"SELECT * FROM crime_freq"
-            print(queryToExecute)
+            # print(queryToExecute)
             cursor.execute(queryToExecute)
             raw_data = cursor.fetchall()
             row_headers = [x[0] for x in cursor.description]
-            print(row_headers)
             json_data = []
 
             for r in raw_data:
@@ -98,27 +109,29 @@ def create_app():
                 'data' : json_data
             }
             
-            print(response)
         except Exception as error:
             response = { 
-                'error' : error 
+                'error' : error,
+                'data': []
             }
             status = 500
         response_pickled = jsonpickle.encode(response)
+        graph['analytics_latency'].observe((time.time_ns() - start) // 1000000)
         return Response(response=response_pickled, status=status, mimetype='application/json')
     
     @app.route('/api/crime_totals', methods=["GET"])
     def get_crime_totals():
+        start = time.time_ns()
+        graph['count'].inc()
         status = 200
         try:
             mydb = getDB()
             cursor = mydb.cursor()
             queryToExecute = "SELECT * FROM crime_totals"
-            print(queryToExecute)
+            #print(queryToExecute)
             cursor.execute(queryToExecute)
             raw_data = cursor.fetchall()
             row_headers = [x[0] for x in cursor.description]
-            print(row_headers)
             json_data = []
 
             for r in raw_data:
@@ -128,21 +141,21 @@ def create_app():
             response = {
                 'data' : json_data
             }
-
-            response = {
-                'data' : json_data
-            }
         except Exception as error:
             response = { 
-                'error' : error 
+                'error' : error,
+                'data' : []
             }
             status = 500
         response_pickled = jsonpickle.encode(response)
+        graph['analytics_latency'].observe((time.time_ns() - start) // 1000000)
         return Response(response=response_pickled, status=status, mimetype='application/json')
 
     @app.route('/api/alldata', methods=["GET"])
     def get_all():
         status = 200
+        start = time.time_ns()
+        graph['count'].inc()
         try:
             mydb = getDB()
             pageno = int(request.args["pageno"])
@@ -155,14 +168,12 @@ def create_app():
             long = float(request.args["long"])
             startTime = int(request.args["startTime"])
             endTime = int(request.args["endTime"])
-            print("reached")
             cursor = mydb.cursor()
             queryToExecute = f"SELECT * FROM crime WHERE REPORTED_DATE < {endTime} AND REPORTED_DATE > {startTime} ORDER BY (POWER(GEO_LAT-{lat}, 2)+POWER(GEO_LON-{long}, 2)) LIMIT {pagesize} OFFSET {(pageno-1)*pagesize}"
-            print(queryToExecute)
+            # print(queryToExecute)
             cursor.execute(queryToExecute)
             raw_data = cursor.fetchall()
             row_headers = [x[0] for x in cursor.description]
-            print(row_headers)
             json_data = []
 
             for r in raw_data:
@@ -183,11 +194,22 @@ def create_app():
             }
         except Exception as error:
             response = { 
-                'error' : error 
+                'error' : error,
+                'data': [],
+                'pageno': 0,
+                'pagesize': 0
             }
             status = 500
         response_pickled = jsonpickle.encode(response)
+        graph['data_latency'].observe((time.time_ns() - start) // 1000000)
         return Response(response=response_pickled, status=status, mimetype='application/json')
+
+    @app.route('/metrics', methods=["GET"])
+    def get_metrics():
+        res = []
+        for k,v in graph.items():
+            res.append(prometheus_client.generate_latest(v))
+        return Response(res, mimetype='text/plain')
     
     return app
 
